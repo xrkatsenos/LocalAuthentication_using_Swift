496f96b2f0d6e644ed510360c8a02c5c7583fc2c
Christos Katsenos <christos.katsenos@ericsson.com>
2020-02-13 16:38:02 +0200

    Initial Commit

---
+++ BiometricAuthentication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BiometricAuthentication
+//
+//  Created by Katsenos Christos on 13/2/20.
+//  Copyright © 2020 Katsenos Christos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
